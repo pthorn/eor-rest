6f65e54dc6febd4177461ab3558e67cf9c6c3c4f
PThorn <p.thorn.ru@gmail.com>
2015-10-14 02:57:55 +0300

    json renderer: ensure_ascii=False for human readable output and fewer bytes

--- eor_rest/json.py
+++ eor_rest/json.py
@@ -10,20 +10,18 @@ def configure_renderer(json):
     def datetime_adapter(obj, request):
         return obj.isoformat()
 
-    def date_adapter(obj, request):
-        return obj.isoformat()
-
-
     def decimal_adapter(obj, request):
         return float(obj)
 
-    json.add_adapter(datetime.date, date_adapter)
+    json.add_adapter(datetime.date, datetime_adapter)
     json.add_adapter(datetime.datetime, datetime_adapter)
     json.add_adapter(decimal.Decimal, decimal_adapter)
 
 
 def get_json_renderer(config):
-    json = JSON()
+    """
+    http://docs.pylonsproject.org/projects/pyramid/en/latest/narr/renderers.html#json-renderer
+    """
+    json = JSON(ensure_ascii=False)
     configure_renderer(json)
     return json
-
