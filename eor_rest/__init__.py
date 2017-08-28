66a839fcd7717e7c7558395cd40aab309502392f
PThorn <p.thorn.ru@gmail.com>
2017-08-28 10:11:05 +0300

    Use Pyramid settings to configure package

--- eor_rest/__init__.py
+++ eor_rest/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from .config import config
+from . import config as config_module
 from .delegate import RestDelegate
 from .views import RestViews
 from .model import RestMixin
@@ -8,6 +8,9 @@ from .exceptions import RESTException, ValidationException
 
 
 def includeme(config):
+    settings = config.get_settings()
+    config_module.config.sqlalchemy_session = settings['eor_rest.sqlalchemy_session']
+
     from .json import get_json_renderer
     config.add_renderer('eor-rest-json', get_json_renderer(config))
 
--- eor_rest/config.py
+++ eor_rest/config.py
@@ -1,11 +1,6 @@
 # coding: utf-8
 
 class Config(object):
-    """
-    from eor_rest import config as rest_config
-    from eor.models import Session
-    rest_config.sqlalchemy_session = Session
-    """
 
     def __init__(self):
         self.sqlalchemy_session = None
